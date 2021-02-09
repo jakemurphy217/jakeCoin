de113254b682c48d6dab305b31e96dd766fe7e3c
jakem <Hondazx123>
2021-02-09 20:02:23 +0000

    mining rewards and multiple transactions

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
+
 //a 'block' in the blockchain 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
+    constructor(timestamp, transactions, previousHash = '') {
 
-        this.index = index;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
@@ -16,8 +24,8 @@ class Block {
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
 
-    mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
             this.nonce++;
             this.hash = this.calculateHash();
         }
@@ -27,40 +35,78 @@ class Block {
 
 // the 'chain' in the blockchain  
 class Blockchain {
-    constructor(){
+    constructor() {
         this.chain = [this.createGenesisBlock()];
         // the amount of 0's infront of the hash - mining 
-        this.difficulty = 5;
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     //first block in the blockchain - never changes index 0
-    createGenesisBlock(){
-        return new Block(0, "01/01/2021", "Genesis Block", "0");
+    createGenesisBlock() {
+        return new Block("01/01/2021", "Genesis Block", "0");
     }
 
     // gets latest block create for seeing previous hash before new block is created.
-    getLatestBlock(){
+    getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    // generates new block for the chain
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress) {
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log("block successfully mined");
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
     }
 
-// checks if chain is valid
-    isChainValid(){
-        for(let i = 1; i < this.chain.length; i++){
+    // pushing transaction to pendig array
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    // getting balance 
+    getBalanceOfAddress(address) {
+        //wallet balance
+        let balance = 0
+
+        // looping through blocks on chain
+        for (const block of this.chain) {
+
+            // looping through transactions wihin each block
+            for (const trans of block.transactions) {
+
+                // fromaddress = minus(-)
+                if (trans.fromAddress === address) {
+                    balance -= trans.amount;
+                }
+                
+                // toaddress = plus(+)
+                if (trans.toAddress === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+        return balance;
+    }
+
+
+    // checks if chain is valid
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
             const previosBlock = this.chain[i - 1];
 
-            if(currentBlock.hash !== currentBlock.calculateHash()){
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
                 return false;
             }
 
-            if(currentBlock.previousHash !== previosBlock.hash){
+            if (currentBlock.previousHash !== previosBlock.hash) {
                 return false;
             }
         }
@@ -70,25 +116,18 @@ class Blockchain {
 
 }
 
-//initating the jakeCoing Blockchain
+//initating the jakeCoin Blockchain
 let jakeCoin = new Blockchain();
 
-console.log('Mining block 1........');
-jakeCoin.addBlock(new Block(1, "01/02/2021", {amount: 4 } ));
+jakeCoin.createTransaction(new Transaction('address1', 'address2', 100));
+jakeCoin.createTransaction(new Transaction('address2', 'address1', 300));
 
-console.log('Mining block 1........');
-jakeCoin.addBlock(new Block(2, "09/02/2021", {amount: 100 } ));
+console.log('\n Starting the miner.....');
+jakeCoin.minePendingTransactions('jakes-address');
 
-// //printing the jakecoin blocks in the terminal
-// console.log(JSON.stringify(jakeCoin, null, 4));
+console.log('\n balance of jakes is: ' + jakeCoin.getBalanceOfAddress('jakes-address'));
 
-// //checks if chain is valid
-// console.log("Is the blockchain valid? " + jakeCoin.isChainValid());
+console.log('\n Starting the miner again.....');
+jakeCoin.minePendingTransactions('jakes-address');
 
-// //changing the values on block to see if changes are detected 
-// jakeCoin.chain[1].data = {amount: 100000000};
-// jakeCoin.chain[1].hash = jakeCoin.chain[1].calculateHash();
-
-// //checking if blockchain is valid again
-// console.log("Is the blockchain valid after change? " + jakeCoin.isChainValid());
-
+console.log('\n balance of jakes is: ' + jakeCoin.getBalanceOfAddress('jakes-address'));
